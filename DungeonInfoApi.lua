e589ec37e7b5a25ce6bb1177ba9ad498a4afe7f5
Matthew Seagle <matthew.seagle@gmail.com>
2018-03-23 00:19:13 -0700

    added in first two functions as well versioning and build information for dapi and wow interface and the first function

--- DungeonInfoApi.lua
+++ DungeonInfoApi.lua
@@ -1,7 +1,17 @@
+--Build info
+function DAPI_Interface() 
+    local version, build, date, tocversion = GetBuildInfo();
+    local DAPIVersion = '0.0.1-ALPHA';
+    local DAPIBuild = '2';
+    local DAPIUpdated = 'March 22 2018';
+    print(string.format("version = %s, build = %s, date = %s, tocversion = %s, DAPIVersion = %s, DAPIBuild = %s,
+        DAPIUpdated = %s.", version, build, date, tocversion, DAPIVersion, DAPIBuild, DAPIUpdated));
+end
+
 --internal functions
 function DAPI_RandomDungeonList() 
-    for i = 1, GetNumRandomDungeons() do 
+    for i = 1, GetNumRandomDungeons() do
         local id, name = GetLFGRandomDungeonInfo(i);
-        print(id, name);
+        print(id, name);        
     end
 end
